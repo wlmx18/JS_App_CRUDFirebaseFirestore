5f925b7a176e3276359e400de2f89d41eb4736cd
wlm.morant <wlm.morant@gmail.com>
2021-02-21 19:33:57 -0500

    createCollection

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const $taskform = document.getElementById('task-form')
 
-$taskform.addEventListener('submit', e => {
+const db = firebase.firestore()
+
+const saveTask = (title, description) => {
+    db.collection('tasks').doc().set({
+        title,
+        description
+    })
+}
+
+$taskform.addEventListener('submit',async e => {
     //console.log('submit ok')
+    e.preventDefault()
     const title = $taskform['task-title'].value
     const description = $taskform['task-description'].value
-    console.log(title, description)
-    e.preventDefault()
+    //console.log(title, description)
+
+    //title:title javascriptModerno solo title
+    await saveTask(title, description)
 })
